b9789e0d188c5c34726c9b0c27b79758091851d7
zenius <zenius2050@gmail.com>
2019-06-29 03:59:01 +0530

    oauth implementation with github

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 // require modules
-const express = require('express'); 
+const express = require('express');
+const path = require('path');
+const appConfig = require('./app-config');
+const oauthRouter = require('./routes/oauth-route');
 
-const app = express(); 
-const port = process.env.PORT || 3000; 
 
-app.get("/", (req, res) => { 
-    res.send("Trying OAuth in Express"); 
-}); 
+const app = express();
+const port = process.env.PORT || 3000;
+const { clientID } = appConfig;
+
+// serve static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// routes
+app.use(oauthRouter);
+
+// landing page route
+app.get('/', (req, res) => {
+  res.render('index', {
+    clientID,
+    port,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`server listening at port ${port}`); 
-}); 
+  console.log(`server listening at port ${port}`);
+});
---
+++ routes/oauth-route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const appConfig = require('../app-config');
+
+const oauthRouter = express.Router();
+const { clientID, clientSecret } = appConfig;
+
+oauthRouter.get('/oauth/redirect', (req, res) => {
+  const { code } = req.query;
+  // make a POST request to get access token
+  axios({
+    method: 'POST',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${code}`,
+    headers: {
+      accept: 'application/json',
+    },
+  }).then((response) => {
+    const { access_token } = response.data;
+    res.redirect('/home-page.html');
+  });
+});
+
+module.exports = oauthRouter;
