4e2dd8c2d8d57df39b3303f316a2a956d566beed
zenius <zenius2050@gmail.com>
2019-07-07 23:19:06 +0530

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  clientID: 'bb5c7eaf566b8393144a',
-  clientSecret: '1ae5118267ef78b8359ae498788a9c080a3241fb',
+  clientID: 'add your client id here',
+  clientSecret: 'add your client secret here',
 };
